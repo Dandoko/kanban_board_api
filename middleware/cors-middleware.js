8813c0336b713d7464c4593f7c8177177fc9f8c4
Daniel Ko <kodaniel2015@gmail.com>
2020-12-06 17:26:04 -0800

    ver0007 cors header

--- middleware/cors-middleware.js
+++ middleware/cors-middleware.js
@@ -4,6 +4,9 @@ const cors = (req, res, next) => {
     res.header("Access-Control-Allow-Methods", "GET, POST, HEAD, OPTIONS, PUT, PATCH, DELETE");
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     
+    // Giving explicit permission to read the headers
+    res.header('Access-Control-Expose-Headers', 'x-access-token, x-refresh-token');
+
     if (req.method === 'OPTIONS') {
         res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
         res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
